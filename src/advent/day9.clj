4c93bba234578949df69d91cbb9f278295fd7a91
Mark Kinsella <mark@kinstelli.com>
2020-12-09 19:32:06 -0600

    day9

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -3,9 +3,7 @@
             [clojure.set :as set]))
 
 (def data (slurp "resources/day9/input.txt"))
-
 (def allnums (map #(Long/parseLong %) (str/split data #"\n")))
-
 (def allnums (take 1000 allnums))
 
 (defn find-addends [numset thesum]
@@ -32,7 +30,7 @@
       (nth allnums endnum)
       (recur (inc startnum) (inc endnum)))))
 
-(find-invalid-nums allnums)
+;(find-invalid-nums allnums)
 
 ;;;;
 ;;;;
@@ -47,14 +45,17 @@
           (println "startnum" startnum "endnum: " endnum)
           (let [thesumresult 100000 #_(apply + (subvec allnums startnum endnum))]
             ;(println "the sum result: " thesumresult)
-            (if (or (>= thesumresult numtofind)
-                    (> endnum (count allnums))) ;(apply + (subvec (vec allnums) startnum endnum)))
-              (do 
-                (println "found it!")
-                #_(subvec allnums startnum endnum))
-              (do 
-                (println "nope!")
-                (recur (inc endnum))) ))))))
+            (cond (> thesumresult numtofind)
+                  (println "sum is too high")
+
+                  (> endnum (count allnums)) ;(apply + (subvec (vec allnums) startnum endnum)))
+                  (println "reached end of numset")
+                  
+                  (= thesumresult numtofind)
+                  (println "found the number!" thesumresult)
+                  
+                  :else
+                    (recur (inc endnum))))))))
 
 
 (def numtofind 26134589)
