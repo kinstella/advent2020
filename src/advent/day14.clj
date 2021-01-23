e6653fb665523f5a254fa5d494d03956d9f5b17c
kinstelli <mark@kinstelli.com>
2021-01-23 13:54:13 -0600

    day14 pt1 solved

--- src/advent/day14.clj
+++ src/advent/day14.clj
@@ -7,9 +7,9 @@
 (def lines (split-lines data))
 
 (defn mask-from-line [maskline]
-  (split maskline #"= "))
+  (last (split maskline #"= ")))
 
-(defn binary-from-memline [memline]
+(defn data-from-memline [memline]
   (let [[addrpart binarypart] (split memline #"] = ")]
     {:addr
      (->  addrpart
@@ -20,20 +20,52 @@
          (Integer/parseInt)
          (Integer/toBinaryString))}))
 
-(defn string-with-bitmask
-  "apply the new value into the current bit string while respecting the mask"
-  [curstr bitmask value])
+(defn lpadx [bin-str len]
+  (str (join  (repeat (- len (count bin-str)) "0")) bin-str))
+
+(defn binstr->bin [binstr]
+  (Long/parseUnsignedLong binstr 2))
+
+(defn sum-memlocs [memvals]
+  (reduce +  (map binstr->bin (vals memvals))))
+
+(defn newval-after-bitmask
+  "apply the new value into the current addr bit string while respecting the mask"
+  [bitmask value]
+  ;(println "-------------------------")
+  (let [paddedval (lpadx value 36)]
+    (join  (map (fn [mask newval]
+                  (if
+
+                   (not= mask \X)
+                    mask
+
+                    newval))
+                bitmask paddedval))))
 
 (defn load-data [lines]
   (loop [remlines lines
-         curval (join (repeat 36 "0"))
+         memvals {}
          curmask (join (repeat 36 "X"))]
     (if (empty? remlines)
-      curval
+      memvals
       (let [x (first remlines)]
         (if (includes? x "mask")
           (do
-            (recur (rest remlines) curval (mask-from-line x)))
+            (recur (rest remlines)
+                   memvals
+                   (mask-from-line x)))
           (do
-            (let [bin-str (binary-from-memline x)]
-              (recur (rest remlines) curval x))))))))
+            (let [bin-str (data-from-memline x)
+                  curval (or (get memvals (:addr bin-str)) (repeat 36 "0"))
+                  newmemval (newval-after-bitmask curmask (:data bin-str))]
+              ;(println "newmemval" newmemval)
+              (recur (rest remlines)
+                     (assoc memvals (:addr bin-str) newmemval)
+                     curmask))))))))
+
+(comment
+  (def memvals (load-data lines))
+  (sum-memlocs memvals)
+  ;; => 10050490168421
+  )
